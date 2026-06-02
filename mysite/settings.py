6cc6d083a06d529c6c9caba4aaabc116693f564b
samsara1126 <samsara26@naver.com>
2026-06-02 17:00:11 +0900

    ch18

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-pz8matx=g)q#v&@4r!z8)sv4dtm+8c9$60eq+jao!t+j!$k#yc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'samsara1126.pythonanywhere.com']
 
@@ -118,3 +118,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media/'
